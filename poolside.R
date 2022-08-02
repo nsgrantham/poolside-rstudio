1188d13262f5614c4e8c7163b60a59c45f7ca5ac
Neal Grantham <neal@nsgrantham.com>
2022-08-01 23:29:41 -0700

    Changes to help docs, borders, dropdown menus

--- poolside.R
+++ poolside.R
@@ -22,8 +22,8 @@ theme_palette <- list(
 )
 
 terminal_colors <- rstheme_terminal_colors(
-  black = "$black",
-  black_bright = "$black",
+  black = "$comment",
+  black_bright = "$comment",
   red = "$red",
   red_bright = "$red",
   green = "$green",
@@ -35,7 +35,7 @@ terminal_colors <- rstheme_terminal_colors(
   magenta = "$pink",
   magenta_bright = "$pink",
   cyan = "$blue",
-  cyan_bright = "blue",
+  cyan_bright = "$blue",
   white = "$white",
   white_bright = "$white"
 )
@@ -62,7 +62,7 @@ rstheme(
   ui_foreground = "$fg",
   ui_cursor = "$bg_lighter",
   ui_invisible = "transparentize($comment, 0.5)",
-  ui_margin_line = "transparentize($comment, 0.5)",
+  ui_margin_line = "transparentize($comment, 0.8)",
   ui_gutter_foreground = "transparentize($comment, 0.3)",
   ui_selection = "lighten($rmd_chunk_background, 5%)",
   ui_rstudio_background = "$bg_darker",
@@ -106,6 +106,105 @@ theme_other <- glue(
   border-color: {{theme_palette$bg}} !important;
 }
 
+/* Syntax highlighting of code in help docs */
+
+.rstudio-themes-dark.editor_dark.ace_editor_theme .code-toolbar {
+  background-color: {{theme_palette$bg}};
+  border: 2px solid {{theme_palette$bg}};
+}
+
+.rstudio-themes-dark.editor_dark.ace_editor_theme pre, 
+.rstudio-themes-dark.editor_dark.ace_editor_theme pre code {
+  background-color: {{theme_palette$bg}};
+  color: {{theme_palette$fg}};
+}
+
+.rstudio-themes-dark.editor_dark.ace_editor_theme .token {
+    filter: brightness(1);
+}
+
+.token.comment,
+.token.prolog,
+.token.doctype,
+.token.cdata {
+  color: {{theme_palette$comment}};
+}
+
+.token.punctuation {
+  color: {{theme_palette$fg_alt}};
+}
+
+.token.property,
+.token.tag,
+.token.number,
+.token.constant,
+.token.symbol,
+.token.deleted {
+	color: {{theme_palette$pink}};
+}
+
+.token.selector,
+.token.attr-name,
+.token.string,
+.token.char,
+.token.builtin,
+.token.inserted {
+	color: {{theme_palette$green}};
+}
+
+.token.operator,
+.token.entity,
+.token.url,
+.language-css .token.string,
+.style .token.string {
+	color: {{theme_palette$fg_alt}};
+}
+
+.token.atrule,
+.token.attr-value,
+.token.boolean,
+.token.keyword {
+	color: {{theme_palette$purple}};
+}
+
+.token.function,
+.token.class-name {
+	color: {{theme_palette$red}};
+}
+
+.token.regex,
+.token.important,
+.token.variable {
+	color: {{theme_palette$yellow}};
+}
+
+/* Column filters in data table view */
+.rstudio-themes-dark-grey .colFilter, 
+.rstudio-themes-dark-grey .numValueBox {
+    border-color: {{ theme_palette$bg_dark }};
+    background: {{ theme_palette$bg_dark }};
+}
+
+/* Border of dropdown menus */
+.rstudio-themes-dark-menus .menuPopupMiddleCenter, .rstudio-themes-dark-menus .themedPopupPanel .popupMiddleCenter, .rstudio-themes-dark-menus .gwt-SuggestBoxPopup .suggestPopupMiddleCenter {
+  border: solid 1px {{ theme_palette$bg_light }};
+}
+
+/* Horizontal seperators in dropdown menus */
+.rstudio-themes-dark-menus .gwt-MenuItemSeparator .menuSeparatorInner {
+  border-color: {{ theme_palette$bg_light }};
+}
+
+/* Border of Source vs Visual toggle button for Rmd docs */
+.rstudio-themes-dark .GND-IWGDIV {
+  border-color: {{ theme_palette$bg_light }};
+}
+
+/* Border of search boxes like 'Go to file/function' and 'Find in Topic' */
+.rstudio-themes-dark-menus .search {
+  border-color: {{ theme_palette$bg_light }};
+}
+
 /* Top border of editor, bottom border of navbar */
 .rstudio-themes-dark .rstheme_toolbarWrapper,
 .rstudio-themes-dark .rstheme_secondaryToolbar {
@@ -138,6 +237,11 @@ theme_other <- glue(
 /* Active tab */
 .rstudio-themes-dark .gwt-TabLayoutPanelTab.gwt-TabLayoutPanelTab-selected .rstheme_tabLayoutCenter {
   border-color: {{theme_palette$bg}} !important;
+}
+
+/* Toolbar when pane is fully collapsed */
+.rstudio-themes-flat .rstudio-themes-dark-grey .rstheme_center {
+  background: {{theme_palette$bg_dark}} !important;
 }
 
 ", .open = "{{", .close = "}}")
